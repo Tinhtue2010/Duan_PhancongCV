e735f822c86f5d5b732839e2932e7518f6d39417
Dat5487 <dat548784@gmail.com>
2025-03-16 09:20:29 +0700

    u

---
+++ app/Http/Controllers/BoPhanController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BoPhan;
+use App\Models\CanBo;
+use App\Models\PhanQuyenBaoCao;
+use App\Models\TaiKhoan;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+
+class BoPhanController extends Controller
+{
+    public function danhSachBoPhan()
+    {
+        $boPhans = BoPhan::all();
+        return view('quan-ly-khac.danh-sach-can-bo', data: compact('data', 'boPhans'));
+    }
+
+    public function themBoPhan(Request $request)
+    {
+        BoPhan::create([
+            'ten_bo_phan' => $request->ten_bo_phan,
+            'chuc_nang_nhiem_vu' => $request->chuc_nang_nhiem_vu,
+            'thoi_gian_thanh_lap' => $request->thoi_gian_thanh_lap,
+            'thoi_gian_giai_the' => $request->thoi_gian_giai_the,
+            'trang_thai' => $request->trang_thai,
+        ]);
+        session()->flash('alert-success', 'Thêm bộ phận mới thành công');
+        return redirect()->back();
+    }
+    public function xoaBoPhan(Request $request)
+    {
+        BoPhan::find($request->ma_bo_phan)->delete();
+        session()->flash('alert-success', 'Xóa bộ phận thành công');
+        return redirect()->back();
+    }
+
+    public function updateBoPhan(Request $request)
+    {
+        $boPhan = BoPhan::find($request->ma_bo_phan);
+        if ($boPhan) {
+            $boPhan->update([
+                'ten_bo_phan' => $request->ten_bo_phan,
+                'chuc_nang_nhiem_vu' => $request->chuc_nang_nhiem_vu,
+                'thoi_gian_thanh_lap' => $request->thoi_gian_thanh_lap,
+                'thoi_gian_giai_the' => $request->thoi_gian_giai_the,
+                'trang_thai' => $request->trang_thai,
+            ]);
+
+            $boPhan->save();
+            session()->flash('alert-success', 'Cập nhật thành công');
+            return redirect()->back();
+        } else {
+            session()->flash('alert-danger', 'Có lỗi xảy ra');
+            return redirect()->back();
+        }
+    }
+}
